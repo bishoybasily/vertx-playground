01d19844e83d5bde3f9ecbf66bd48dbd0cba7da0
bishoybasily <bishoybasily@gmail.com>
2021-06-04 05:00:41 +0200

    keycloak demo

--- src/main/java/com/gmail/bishoybasily/VerticleHttpServer.java
+++ src/main/java/com/gmail/bishoybasily/VerticleHttpServer.java
@@ -6,9 +6,13 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.oauth2.OAuth2FlowType;
+import io.vertx.ext.auth.oauth2.providers.KeycloakAuth;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.ext.web.handler.LoggerHandler;
+import io.vertx.ext.web.handler.OAuth2AuthHandler;
 
 /**
  * @author bishoybasily
@@ -31,11 +35,31 @@ public class VerticleHttpServer extends AbstractVerticle {
     private Future<HttpServer> startHttpServer() {
         var router = Router.router(vertx);
 
-        router.route().handler(LoggerHandler.create());
-        router.route().handler(CorsHandler.create());
+        var loggerHandler = LoggerHandler.create();
+        var corsHandler = CorsHandler.create();
+        var oAuth2AuthHandler = OAuth2AuthHandler.create(vertx, KeycloakAuth.create(vertx, OAuth2FlowType.AUTH_CODE, new JsonObject("{\n" +
+                "  \"realm\": \"master\",\n" +
+                "  \"auth-server-url\": \"http://127.0.0.1:38080/auth/\",\n" +
+                "  \"ssl-required\": \"external\",\n" +
+                "  \"resource\": \"vertx\",\n" +
+                "  \"credentials\": {\n" +
+                "    \"secret\": \"e44d6419-890b-4889-9323-b61b2dc2aa96\"\n" +
+                "  },\n" +
+                "  \"confidential-port\": 0\n" +
+                "}")), "http://localhost:8080/keycloak")
+                .setupCallback(router.get("/keycloak"));
 
-        router.get("/api/welcome").handler(ctx -> {
-            vertx.eventBus().<String>request("api.welcome", "", reply -> {
+        router.route().handler(loggerHandler);
+        router.route().handler(corsHandler);
+        router.route("/private/*").handler(oAuth2AuthHandler);
+
+        router.get("/public/welcome").handler(ctx -> {
+            vertx.eventBus().<String>request("public.welcome", "", reply -> {
+                ctx.response().end(reply.result().body());
+            });
+        });
+        router.get("/private/welcome").handler(ctx -> {
+            vertx.eventBus().<String>request("private.welcome", "", reply -> {
                 ctx.response().end(reply.result().body());
             });
         });
--- src/main/java/com/gmail/bishoybasily/VerticleWelcome.java
+++ src/main/java/com/gmail/bishoybasily/VerticleWelcome.java
@@ -16,8 +16,11 @@ public class VerticleWelcome extends AbstractVerticle {
     @Override
     public void start(Promise<Void> promise) {
 
-        vertx.eventBus().consumer("api.welcome", it -> {
+        vertx.eventBus().consumer("public.welcome", it -> {
             it.reply(String.format("Hello from vertx another verticle typed threaded %s", id));
+        });
+        vertx.eventBus().consumer("private.welcome", it -> {
+            it.reply(String.format("This is a very secured api, how did you get there! %s", id));
         });
 
         promise.complete();
